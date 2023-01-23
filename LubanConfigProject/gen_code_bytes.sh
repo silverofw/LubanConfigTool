8a22eab4e307ccfdbd04395187c3cdf45bb220ab
Silver <silverofw@gmail.com>
2023-01-24 00:08:15 +0800

    [UPDATE]  mac .sh

--- LubanConfigProject/gen_code_bytes.sh
+++ LubanConfigProject/gen_code_bytes.sh
@@ -10,6 +10,6 @@ dotnet ${GEN_CLIENT} -h ${LUBAN_SERVER_IP} -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_code_dir ${WORKSPACE}/LocalPackages/com.focuscreativegames.luban/Gen \
- --output_data_dir ${WORKSPACE}/Assets/Res/Configs/json \
- --gen_types code_cs_unity_json,data_json \
+ --output_data_dir ${WORKSPACE}/Assets/Res/Configs/bytes \
+ --gen_types code_cs_unity_bin,data_bin \
  -s all 
